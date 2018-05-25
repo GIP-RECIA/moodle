178bcab68fdf5cdbd2e8a11ab176aeaf9dbb11e3
Pierre LEJEUNE <pierre.lejeune@gfi.fr>
2018-05-25 09:42:16 +0200

    Correction problème comportement menu principal si utilisateur non connecté

--- lib/navigationlib.php
+++ lib/navigationlib.php
@@ -1435,7 +1435,7 @@ class global_navigation extends navigation_node {
 
         // If the user is not logged in modify the navigation structure as detailed
         // in {@link http://docs.moodle.org/dev/Navigation_2.0_structure}
-        if (!isloggedin()) {
+        if (!isloggedin() && !is_null($this->rootnodes['site']->children)) {
             $activities = clone($this->rootnodes['site']->children);
             $this->rootnodes['site']->remove();
             $children = clone($this->children);
